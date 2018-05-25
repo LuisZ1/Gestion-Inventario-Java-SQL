3e84003e3c99f9007fd70a522441fcd782478c33
LuisZ1 <luiszumarraga95@gmail.com>
2018-05-25 12:05:15 +0200

    mensajes añadida doc

--- src/menuAndMessages/Messages.java
+++ src/menuAndMessages/Messages.java
@@ -6,13 +6,17 @@ import java.util.ResourceBundle;
 
 public class Messages {
 		
+	/** Nombre del fichero donde están los mensajes */
 	private static String BUNDLE_NAME = "messages.messages"; 
 
+	/** */
 	private static ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
-	private Messages() {
-	}
-
+	/**
+	 * El metodo obtiene el mensaje que hay que mostrar del fichero anteriormente declarado
+	 * @param key El identificador del mensaje
+	 * @return el mensaje con el identificador enviado
+	 */
 	public static String getString(String key) {
 		try {
 			return RESOURCE_BUNDLE.getString(key);
@@ -21,6 +25,10 @@ public class Messages {
 		}
 	}
 	
+	/**
+	 * El método sirve para cambiar el idioma, dependiendo del valor asociado a la variable Bundle_Name
+	 * @param lang el idioma al que se desea cambiar
+	 */
 	public static void changeLanguage(String lang) {
 		if(lang.toLowerCase().equals("es")) {
 			BUNDLE_NAME = "messages.messages_es_ES";
